a502360ea38bd72726bfe36dcf188a2d3afa1853
Liko Indra Hartawan <liko.hartawan@ist.id>
2024-08-21 10:48:50 +0700

    Implement Complit Layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,13 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  const loggedIn = { firstName: "Indra" };
+  const loggedIn = {
+    firstName: "Indra",
+    lastName: "Liko",
+    email: "test@gmail.zxc",
+  };
   return (
     <section className="home">
       <div className="home-content">
@@ -13,7 +19,18 @@ const Home = () => {
             subtext="Access and manage your account and transactions effiencently."
           />
         </header>
+        <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.35}
+        />
       </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transaactions={[]}
+        banks={[{ currentBalance: 123.45 }, { currentBalance: 200 }]}
+      />
     </section>
   );
 };
